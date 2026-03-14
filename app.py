497ae868d540c18f71d6da39c28a08f11451d036
Purnachander602 <purnachander7569@gmail.com>
2026-03-14 09:31:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,33 @@
 import streamlit as st
-import ollama
+from groq import Groq
 
-st.set_page_config(page_title="AI Developer Assistant", layout="wide")
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 st.title("💻 AI Developer Assistant")
 
-# Store chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Show previous messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# User input
 prompt = st.chat_input("Ask coding question...")
 
 if prompt:
-
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    response = client.chat.completions.create(
+        model="llama3-70b-8192",
+        messages=st.session_state.messages
+    )
+
+    answer = response.choices[0].message.content
+
     with st.chat_message("assistant"):
-
-        response = ollama.chat(
-            model="deepseek-coder",
-            messages=st.session_state.messages
-        )
-
-        answer = response["message"]["content"]
-
         st.markdown(answer)
 
     st.session_state.messages.append(
